ffbaa7e16ccf451c6369602f60e8a5a0a83f21e0
sivanambivelu <119469097+sivanambivelu@users.noreply.github.com>
2022-12-16 13:39:33 +0900

    Delete s3.upload_file.py

--- s3.upload_file.py
+++
@@ -1,9 +0,0 @@
-s3.upload_file(
-	aws_access_key_id = AKIA453UTWFYMXMJQLV2
-	aws_secret_access_key = Rk7dkkqKispUTl1rrsL+N3ObNs9aQxXlCL+K/ygr
-	output = json
-	region = us-east-2
-    Filename="data/downloaded_from_s3.csv",
-    Bucket="sample-bucket-1801",
-    Key="new_file.csv",
-)
